e96a65186c099c2bbd5a91930747d7bb43767fc2
Deepak Sharma <uddibhardwaj08@gmail.com>
2023-02-10 17:56:04 +0530

    day3

--- day3/Index.js
+++ day3/Index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+console.log(path.join(__dirname, '/public'));
+const staticPath = path.join(__dirname, '/public')
+app.use(express.static(staticPath));
 app.get('/', (req, res) => {
     res.send('<h1>hello this is home page</h1>');
 })
