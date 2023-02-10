9ee5119359424d8e212e98f4229ca4e49c8d428e
Deepak Sharma <uddibhardwaj08@gmail.com>
2023-02-10 18:14:52 +0530

    day3

--- day3/Index.js
+++ day3/Index.js
@@ -1,11 +1,25 @@
-const express = require('express');
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// console.log(path.join(__dirname, '/public'));
+// const staticPath = path.join(__dirname, '/public')
+// app.use(express.static(staticPath));
+// app.get('/', (req, res) => {
+//     res.send('<h1>hello this is home page</h1>');
+// })
+// app.listen(8000, () => {
+//     console.log('<h1>Listening on port</h1>');
+// })
+const express = require('express')
 const path = require('path');
 const app = express();
-console.log(path.join(__dirname, '/public'));
-const staticPath = path.join(__dirname, '/public')
-app.use(express.static(staticPath));
+const staticPath = path.join(__dirname, '/temp')
+app.set('view engine', 'hbs');
+app.set('view engine', staticPath);
 app.get('/', (req, res) => {
-    res.send('<h1>hello this is home page</h1>');
+    res.render('index', {
+        newdata: 'Udit Sharma'
+    });
 })
 app.listen(8000, () => {
     console.log('<h1>Listening on port</h1>');
