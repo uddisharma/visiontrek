0dcb6a71764f9497e13f645dfd953d27594f7e84
Deepak Sharma <uddibhardwaj08@gmail.com>
2023-02-10 21:03:02 +0530

    day3

--- day3/Index.js
+++ day3/Index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
 app.get('/', (req, res) => {
     res.render('index', {
-        name: 'Udit Sharma'
+        name: 'Udit Sharma bhardwaj gamdiwal '
     })
 })
-app.listen(2000, () => {
+app.listen(1000, () => {
     console.log('im listening');
-})
+})  
