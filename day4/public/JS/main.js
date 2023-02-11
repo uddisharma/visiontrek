246537edf9c8be5eab7ce0d52d8313ccd513c539
Deepak Sharma <uddibhardwaj08@gmail.com>
2023-02-11 19:20:16 +0530

    day4

---
+++ day4/public/JS/main.js
@@ -0,0 +1,7 @@
+var btn= document.getElementById('seachbtn')
+btn.addEventListener('click',search)
+const api= 'https://api.openweathermap.org/data/2.5/weather?q=pune&appid=cfafec3ac99033ff40c52e4bbae91dab'
+function search(){
+    alert('Search')
+    
+}
