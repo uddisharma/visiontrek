ef3f070ae20fbe361c025a3d63dde1f1ce669a2d
Deepak Sharma <uddibhardwaj08@gmail.com>
2023-02-11 13:42:10 +0530

    day4

--- day4/src/app.js
+++ day4/src/app.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const path = require('path');
+const hbs= require('hbs');
 const app = express();
 
 const staticpath = path.join(__dirname, '../public');
-app.use(express.static(staticpath))
-app.get('/', (req, res) => {
-    res.send('this is home page');
+const newpath=path.join(__dirname,'../templates/views');
+const partials=path.join(__dirname,'../templates/partials');
+app.set('view engine', 'hbs');
+app.set('views', newpath)
+hbs.registerPartials(partials)
+// app.use(express.static(staticpath))
+app.get('/',(rq,res)=>{
+    res.render('index')
 })
 app.get('/about', (req, res) => {
-    res.send('this is about page');
+    res.render('about');
+})
+app.get('/weather', (req, res) => {
+    res.render('weather');
 })
 app.get('*', (req, res) => {
-    res.send('sorry page is not available');
+    res.render('404');
 })
 app.listen(8000, () => {
     console.log('Listening on port')
