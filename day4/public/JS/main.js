82969651e422f5a02cc9cf3da4091bd3c145ebaa
Deepak Sharma <uddibhardwaj08@gmail.com>
2023-02-12 22:07:10 +0530

    day4

--- day4/public/JS/main.js
+++ day4/public/JS/main.js
@@ -1,7 +1,23 @@
 var btn= document.getElementById('seachbtn')
+var value= document.getElementById('cityname')
+var finalres= document.getElementById('finalres');
 btn.addEventListener('click',search)
-const api= 'https://api.openweathermap.org/data/2.5/weather?q=pune&appid=cfafec3ac99033ff40c52e4bbae91dab'
-function search(){
+
+  function search (){
     alert('Search')
-    
+    // console.log('Search is working')
+    // let cityvalue = value.value;
+    //  if(cityvalue==''){
+    //     finalres.innerText='Please enter a city name'
+    //  }else{
+    //     try {
+    //         var api=`http://api.openweathermap.org/data/2.5/weather?q=jind&units=metric&appid=cfafec3ac99033ff40c52e4bbae91dab`
+    //   var res= await fetch(api)
+    //     } catch (error) {
+    //         console.log(error)
+    //         finalres.innerText='please enter a city name  before search '
+    //     }
+     
+    //  }
 }
+
