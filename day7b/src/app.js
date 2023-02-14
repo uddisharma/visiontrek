35a14e805b9090d4cf4f08ef158ca382025b3b21
Deepak Sharma <uddibhardwaj08@gmail.com>
2023-02-14 21:50:40 +0530

    day7b

--- day7b/src/app.js
+++ day7b/src/app.js
@@ -8,9 +8,17 @@ app.get("/", (req, res) => {
   res.send("this is a test");
 });
 app.post("/students", (req, res) => {
-  res.send("this is a test for sending the data");
-  //   const data = new Student(req.body);
+  //   res.send("this is a test for sending the data");
   console.log(req.body);
+  const data = new Student(req.body);
+  data
+    .save()
+    .then(() => {
+      res.status(201).send(data);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
 });
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
--- day7b/src/modals/student.js
+++ day7b/src/modals/student.js
@@ -8,29 +8,31 @@ const studentSchema = new mongoose.Schema({
   age: {
     type: Number,
     required: true,
-    validate(value) {
-      if (value < 18) {
-        throw new Error("Please enter a valid age.");
-      }
-    },
-    email: {
-      type: String,
-      required: true,
-      validate(value) {
-        if (!validator.isEmail(value)) {
-          throw new Error("Please enter a valid email");
-        }
-      },
-    },
-    number: {
-      type: Number,
-      required: true,
-      validate(value) {
-        if (!validator.isMobilePhone(value)) {
-          throw new Error("Please enter a valid number");
-        }
-      },
-    },
+    // validate(value) {
+    //   if (value < 18) {
+    //     throw new Error("Please enter a valid age.");
+    //   }
+    // }
+  },
+  email: {
+    type: String,
+    required: true,
+    //   unique: true,
+    //   validate(value) {
+    //     if (!validator.isEmail(value)) {
+    //       throw new Error("Please enter a valid email");
+    //     }
+    //   },
+  },
+  number: {
+    type: Number,
+    required: true,
+    //   unique: true,
+    //   validate(value) {
+    //     if (!validator.isMobilePhone(value)) {
+    //       throw new Error("Please enter a valid number");
+    //     }
+    //   },
   },
 });
 
